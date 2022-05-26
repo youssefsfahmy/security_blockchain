e3fd6f8d86ed3094bb177ef5903542bde5fc958f
youssefsfa7my <youssef.samir@student.guc.edu.eg>
2022-05-26 15:55:53 +0200

    initial

---
+++ api/routers/doctors.router.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const router = express.Router()
+
+const doctorsController = require('../controllers/doctors.controller')
+const cryptoController = require('../controllers/crypto.controller')
+
+// const {
+//   createPricing,
+//   deletePricing,
+//   editPricing,
+//   viewPricings,
+// } = pricingController
+
+// const {
+//   validateCreatePricing,
+//   validateDeletePricing,
+//   validateEditPricing,
+//   validateViewPricing,
+// } = require('../helpers/validations/pricingValidation')
+
+// const { verifyToken } = require('../../config/AuthenticationMiddleWare')
+// const { verifyAdmin } = require('../../config/AdminAuthentication')
+// const { verifyUser } = require('../../config/authUser')
+
+// router.post('/createPricing', validateCreatePricing, verifyAdmin, createPricing)
+// router.post('/editPricing', validateEditPricing, verifyAdmin, editPricing)
+// router.post('/deletePricing', validateDeletePricing, verifyAdmin, deletePricing)
+// router.post(
+//   '/viewPricings',
+//   validateViewPricing,
+//   verifyToken,
+//   verifyUser,
+//   viewPricings
+// )
+
+module.exports = router
